8038abcf359257c7bf508f462c5ad95f9fe90dce
Matteo Nastasi <nastasi@openquake.org>
2016-05-16 13:27:50 +0000

    conditional PYTHONPATH assignment (enhanced)

--- runserver.sh
+++ runserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ "$PYTHONPATH" = "" ]; then
-    export PYTHONPATH="$PWD"
+if ! (echo "$PYTHONPATH" | grep -q ":\?${PWD}:\?" ); then
+    export PYTHONPATH="${PYTHONPATH}:${PWD}"
 fi
 cd ../oq-platform-standalone
 ./runserver.sh
