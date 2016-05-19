c4b09768442a7dc263ee2f53957eff541a7b15f0
Matteo Nastasi <nastasi@openquake.org>
2016-05-19 08:56:07 +0000

    add setTimeout() as partial workaround of table height recalcuation where missing

--- openquakeplatform_ipt/static/ipt/js/fragility.js
+++ openquakeplatform_ipt/static/ipt/js/fragility.js
@@ -170,6 +170,10 @@ function ff_updateTable (funcType) {
         var tbl = ff_obj.tbl[ff_obj.tbl_idx];
         var $box = $('.ff_gid [name="tableDiv'+ff_obj.tbl_idx+'"]');
 
+        setTimeout(function() {
+            return gem_tableHeightUpdate(tbl, $box);
+        }, 0);
+
         tbl.addHook('afterCreateRow', function() {
             return gem_tableHeightUpdate(tbl, $box);
         });
