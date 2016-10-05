7e85db520185efd2db0191b45a31bd6864ed8206
Matteo Nastasi <nastasi@openquake.org>
2016-10-05 16:22:46 +0200

    add a common gem_ipt object with common methods of ipt app

--- openquakeplatform_ipt/static/ipt/js/common.js
+++ openquakeplatform_ipt/static/ipt/js/common.js
@@ -57,6 +57,97 @@ function gem_capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+var gem_ipt = {
+    check_exception: function(message) {
+        this.message = message;
+        this.name = "CheckException";
+    },
+
+    isInt: function(n) {
+        return !isNaN(n) && n % 1 === 0 && !(n === null);
+    },
+
+    isFloat: function(n) {
+        return  !/^\s*$/.test(n) && !isNaN(n) && !(n === null);
+    },
+
+    check_val: function (name, val, oper)  {
+        console.log("Name: " + name + "  Val: " + val + " Oper: " + oper);
+        /* is empty ? */
+        if (val == "")
+            throw new this.check_exception("'" + name + "' field is empty");
+
+        /* type check */
+        if (oper.substr(0,6) == "float-") {
+            if (!this.isFloat(val))
+                throw new this.check_exception("'" + name + "' field isn't a float number (" + val + ").");
+        }
+        else if (oper.substr(0,4) == "int-") {
+            if (!this.isInt(val))
+                throw new this.check_exception("'" + name + "' field isn't an integer number (" + val + ").");
+        }
+
+        if (oper == "float-ge") {
+            var second = parseFloat(arguments[3]);
+
+            val = parseFloat(val);
+            if (!(val >= second))
+                throw new this.check_exception("'" + name + "' field is less than " + second + " (" + val + ").");
+            return val;
+        }
+        if (oper == "float-gt") {
+            var second = parseFloat(arguments[3]);
+
+            val = parseFloat(val);
+            if (!(val > second))
+                throw new this.check_exception("'" + name + "' field isn't great than " + second + " (" + val + ").");
+            return val;
+        }
+        else if (oper == "float-range-in-in") {
+            var second = parseFloat(arguments[3]);
+            var third = parseFloat(arguments[4]);
+            if (!(second <= val && val <= third))
+                throw new this.check_exception("'" + name + "' field not in [" + second + ", " + third + "] range (" + val + ").");
+        }
+        else if (oper == "float-range-out-in") {
+            var second = parseFloat(arguments[3]);
+            var third = parseFloat(arguments[4]);
+            if (!(second < val && val <= third))
+                throw new this.check_exception("'" + name + "' field not in (" + second + ", " + third + "] range (" + val + ").");
+        }
+        else if (oper == "tab-check") {
+            var descr_cols = arguments[3], descr_rows = null;
+            if (arguments.length > 4) {
+                var descr_rows = arguments[4];
+            }
+            for (var i = 0 ; i < (descr_rows == null ? val.length : descr_rows.length) ; i++) {
+                if (val[i].length != descr_cols.length)
+                    throw new this.check_exception("Wrong number of columns in '" + nome + "' at line " +
+                                                   (i + 1) +". Expected " +
+                                                   descr_cols.length + " received " + val[i].length);
+                for (e = 0 ; e < val[i].length ; e++) {
+                    try {
+                        var args = [ descr_cols[e][0], val[i][e] ].concat(descr_cols[e].slice(1));
+                        var ret = this.check_val.apply(this, args);
+                    } catch(exc) {
+                        throw new this.check_exception("Error at row "
+                                                       + (descr_rows == null ? (i + 1) : "'" + descr_rows[i] + "'")
+                                                       + " with message:\n" + exc.message);
+                    }
+                }
+            }
+            if (descr_rows != null) {
+                if (i != descr_rows.length) {
+                    throw new this.check_exception("Error in '" + name + "': the number of rows isn't " +
+                                                   descr_rows.length + ".");
+                }
+            }
+        }
+        else {
+            throw new this.check_exception("Operator '" + oper + "' not yet implemented.");
+        }
+    }
+}
 
 function isInt(n){
     return !isNaN(n) && n % 1 === 0;
