d328f1a2281c0fe7b97c21cef52988eb2ea3721d
Matteo Nastasi <nastasi@openquake.org>
2016-10-13 15:58:57 +0200

    update version for development

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='oq-platform-ipt',
-    version='1.1.0',
+    version='1.2.0',
     packages=["openquakeplatform_ipt"],
     include_package_data=True,
     license='BSD License',  # example license
