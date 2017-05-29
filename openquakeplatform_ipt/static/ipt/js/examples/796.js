a10dab068ac4a1e88b4140d7de2eeb7ddd5b5072
Matteo Nastasi <nastasi@openquake.org>
2017-05-29 15:44:40 +0200

    cleanup

--- openquakeplatform_ipt/static/ipt/js/examples/796.js
+++ openquakeplatform_ipt/static/ipt/js/examples/796.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    console.log('inizio');
     $('a[href="#subtabs-3"]').click();
     $(cf_obj['e_b'].pfx + ' div[name="exposure-model-html"] select[name="file_html"]').val('exposure_model/exposure_model.xml');
     $(cf_obj['e_b'].pfx + ' div[name="vulnerability-model"] input[type="checkbox"][name="losstype"][value="occupants"]').prop('checked', true).triggerHandler('click');
@@ -22,20 +21,5 @@ $(document).ready(function () {
 
     $(cf_obj['e_b'].pfx + ' div[name="hazard-sitecond_from-file"] select[name="file_html"]').val('site_conditions/site_model_surrogate.xml');
 
-
-    
-    //$(cf_obj['e_b'].pfx + ' div[name="exposure-model-html"] button:contains("Upload")').submit();
-    /*
-    $(cf_obj['e_b'].pfx + ' div[name="exposure-model-html"] select[name="file_html"]'
-     ).val('data/exposure_model/exposure_model.xml');
-    $(cf_obj['e_b'].pfx + ' div[name="vm-structural-html"] select[name="file_html"]'
-     ).val('data/vulnerability_model/structural_vulnerability_model.xml');
-    $(cf_obj['e_b'].pfx + ' div[name="source-model-logic-tree-file-html"] select[name="file_html"]'
-     ).val('data/source_model_logic_tree_file/source_model.xml');
-    $(cf_obj['e_b'].pfx + ' div[name="gsim-logic-tree-file-html"] select[name="file_html"]'
-     ).val('data/gsim_logic_tree_file/gmpe_logic_tree.xml');
-    $(cf_obj['e_b'].pfx + ' div[name="hazard-model"] div[name="source-model-file-html"] select').val(['data/source_model_file/source_model01.xml']);
-
-    $(cf_obj['e_b'].pfx + ' button[name="download"]').click();
-*/
+    // $(cf_obj['e_b'].pfx + ' button[name="download"]').click();
 });
