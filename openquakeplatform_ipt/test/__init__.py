48aff3512aa2329ce2f5ef85d4543abbc44f9509
Matteo Nastasi <nastasi@openquake.org>
2017-11-15 09:04:24 +0100

    update moon platform management

--- openquakeplatform_ipt/test/__init__.py
+++ openquakeplatform_ipt/test/__init__.py
@@ -1,14 +1,12 @@
-from openquake.moon import platform_get
-from .ipt_test import IptTest
-from .examples_test import IptExamplesTest, IptUploadTest
+from openquake.moon import platform_get, platform_del
+# from .ipt_test import IptTest
+# from .examples_test import IptExamplesTest, IptUploadTest
+
 
 def setup_package():
     pla = platform_get()
-    print("setup_package: ipt")
     pla.init(autologin=False)
 
 
 def teardown_package():
-    pla = platform_get()
-    print("teardown_package: ipt")
-    pla.fini()
+    platform_del()
