9fe1f5ef8995bf23d54f89e3f03e2a0b0bc0d541
Armando Scarpati <armando.scarpati@globalquakemodel.org>
2018-02-23 15:22:42 +0100

    corrected syntax error

--- openquakeplatform_ipt/static/ipt/js/examples/7993.js
+++ openquakeplatform_ipt/static/ipt/js/examples/7993.js
@@ -22,28 +22,4 @@ $(document).ready(function () {
         $(cf_obj['e_b'].pfx + ' button[name="download"]').click();
         window.gem_example_completed = true;
     }, 1000);
-})edy(function () {
-    window.gem_example_completed = false;
-    // this is a workaround for a bug fixed in jquery 1.9 (checked is toggled after that handler is fired
-    $(cf_obj['e_b'].pfx + " div[name='exposure-model-new'] input[name='file_upload']").submit();
-    $(cf_obj['e_b'].pfx + " div[name='vm-structural-new'] input[name='file_upload']").submit();
-    $(cf_obj['e_b'].pfx + " div[name='source-model-logic-tree-file-new'] input[name='file_upload']").submit();
-    $(cf_obj['e_b'].pfx + " div[name='source-model-file-new'] input[name='file_upload']").submit();
-    $(cf_obj['e_b'].pfx + " div[name='gsim-logic-tree-file-new'] input[name='file_upload']").submit();
-
-    setTimeout(function () {
-        $(cf_obj['e_b'].pfx + ' div[name="exposure-model-html"] select[name="file_html"]').val('exposure_model/exposure_model.xml');
-        $(cf_obj['e_b'].pfx + ' div[name="vm-structural-html"] select[name="file_html"]').val('vulnerability_model/vulnerability_model_BOG.xml');
-        $(cf_obj['e_b'].pfx + ' div[name="source-model-logic-tree-file-html"] select[name="file_html"]').val('source_model_logic_tree_file/source_model_logic_tree.xml');
-        $(cf_obj['e_b'].pfx + ' div[name="source-model-file-html"] select[name="file_html"]').val('source_model_file/int_col_bog.xml');
-        $(cf_obj['e_b'].pfx + ' div[name="gsim-logic-tree-file-html"] select[name="file_html"]').val('gsim_logic_tree_file/gmpe_logic_tree.xml');
-
-        // Click check rupture mesh spacing and area source discretization
-        $(cf_obj['e_b'].pfx + ' input[type="checkbox"][name="rupture_mesh_spacing_choice"]').prop('checked', true).triggerHandler('click');
-        $(cf_obj['e_b'].pfx + ' input[type="checkbox"][name="area_source_discretization_choice"]').prop('checked', true).triggerHandler('click');
-
-        // Click to download EventBase.zip
-        $(cf_obj['e_b'].pfx + ' button[name="download"]').click();
-        window.gem_example_completed = true;
-    }, 1000);
 });
