1c17443ed8fedda854fc38f53e21a8114eab00c2
Matteo Nastasi <nastasi@openquake.org>
2019-12-10 14:07:43 +0100

    replaced double quotes with single quotes to assign csrf_token from csrf middleware

--- openquakeplatform_ipt/templates/ipt/ipt.js
+++ openquakeplatform_ipt/templates/ipt/ipt.js
@@ -2,7 +2,7 @@ var tab_id = 0, cf_subtab_id = 0;
 var multi_accept = {{ multi_accept|safe }};
 
 help = document.getElementById("help_url");
-csrf_token = "{% csrf_token %}";
+csrf_token = '{% csrf_token %}';
 
 if(help != null) {
     help.href+='/explore/';
