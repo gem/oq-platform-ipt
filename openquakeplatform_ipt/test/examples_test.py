2167a372d4de00f35a4c961207ca1ae1e6b9eaee
Matteo Nastasi <nastasi@openquake.org>
2020-02-04 12:22:25 +0100

    change test conditions

--- openquakeplatform_ipt/test/examples_test.py
+++ openquakeplatform_ipt/test/examples_test.py
@@ -57,7 +57,7 @@ imt_examples = {
              'xpath': ["//div[@id='tabs-1']//div[@id='validationErrorMsg']"],
              'sfx': 'txt'},
             {'exa_id': 99, 'subtab_id': 0,
-             'xpath': ["//div[@id='tabs-1']//div[@id='validationErrorMsg']"],
+             'xpath': ["//div[@id='tabs-1']//div[@id='validationErrorMsg']",
                        "//textarea[@id='textareaex']"],
              'sfx': 'xml'}
         ]
